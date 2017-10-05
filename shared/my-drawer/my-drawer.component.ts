b56e43e572341b4a47bbf001e6a396ecac201db6
Martin Yankov <m.i.yankov@gmail.com>
2017-10-05 18:01:08 +0300

    Simplify drawer content.

--- shared/my-drawer/my-drawer.component.ts
+++ shared/my-drawer/my-drawer.component.ts
@@ -20,54 +20,14 @@ export class MyDrawerComponent implements OnInit {
     *************************************************************/
     @Input() selectedPage: string;
 
-    private _navigationItems: Array<any>;
-
     constructor(private routerExtensions: RouterExtensions) {
 
     }
 
-    /* ***********************************************************
-    * Use the MyDrawerComponent "onInit" event handler to initialize the properties data values.
-    * The navigationItems property is initialized here and is data bound to <ListView> in the MyDrawer view file.
-    * Add, remove or edit navigationItems to change what is displayed in the app drawer list.
-    *************************************************************/
     ngOnInit(): void {
-        this._navigationItems = [
-            {
-                title: "Home",
-                name: "home",
-                route: "/home",
-                icon: "\uf015"
-            },
-            {
-                title: "Browse",
-                name: "browse",
-                route: "/browse",
-                icon: "\uf1ea"
-            },
-            {
-                title: "Search",
-                name: "search",
-                route: "/search",
-                icon: "\uf002"
-            },
-            {
-                title: "Featured",
-                name: "featured",
-                route: "/featured",
-                icon: "\uf005"
-            },
-            {
-                title: "Settings",
-                name: "settings",
-                route: "/settings",
-                icon: "\uf013"
-            }
-        ];
-    }
-
-    get navigationItems(): Array<any> {
-        return this._navigationItems;
+        /* ***********************************************************
+        * Use the MyDrawerComponent "onInit" event handler to initialize the properties data values.
+        *************************************************************/
     }
 
     /* ***********************************************************
@@ -75,11 +35,8 @@ export class MyDrawerComponent implements OnInit {
     * The "itemTap" event handler of the app drawer <ListView> is used to navigate the app
     * based on the tapped navigationItem's route.
     *************************************************************/
-    onNavigationItemTap(args: ItemEventData): void {
-        const navigationItemView = args.view;
-        const navigationItemRoute = navigationItemView.bindingContext.route;
-
-        this.routerExtensions.navigate([navigationItemRoute], {
+    onNavItemTap(navItemRoute: string): void {
+        this.routerExtensions.navigate([navItemRoute], {
             transition: {
                 name: "fade"
             }
